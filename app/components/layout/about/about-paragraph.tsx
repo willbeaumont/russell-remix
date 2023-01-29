089242aa6e4f4aabffb17e4bf218d3df68a65e50
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2023-01-29 09:44:33 -0800

    add dir for about components

---
+++ app/components/layout/about/about-paragraph.tsx
@@ -0,0 +1,39 @@
+import { Link } from "@remix-run/react";
+import { ContentData, SiteData } from "~/data";
+
+interface AboutParagraphProps {
+  tagline: string;
+  link: string;
+  blurb: string;
+}
+
+export function Paragraph({ tagline, link, blurb }: AboutParagraphProps) {
+  return (
+    <div className="mx-14 lg:mx-40 my-10 sm:my-16 sm:text-xl lg:text-2xl">
+      <strong>
+        <Link to={link} className="text-about-link hover:underline">
+          {tagline}
+        </Link>
+      </strong>
+      <span className="text-about-text"> {blurb}</span>
+    </div>
+  );
+}
+
+export default function About({ data }: { data: SiteData }) {
+  return (
+    <section id="about-content" className="mt-24 lg:mt-28">
+      {data.about.content.map(
+        (section) =>
+          section && (
+            <Paragraph
+              key={section.key}
+              tagline={section.tagline}
+              link={section.link}
+              blurb={section.blurb}
+            />
+          )
+      )}
+    </section>
+  );
+}
