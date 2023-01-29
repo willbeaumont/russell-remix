f3c4623fc7d22f4edba7bdbf62bd79fc0a60e2d1
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2023-01-29 13:14:39 -0800

    refactor build default

---
+++ app/components/layout/building.tsx
@@ -0,0 +1,7 @@
+export default function BuildingPlaceholder() {
+  return (
+    <div className="flex h-full w-full p-14 items-center justify-center text-4xl">
+      ⛑️ This page is still under construction...
+    </div>
+  );
+}
--- app/routes/interactive-design/index.tsx
+++ app/routes/interactive-design/index.tsx
@@ -1,7 +1,5 @@
+import BuildingPlaceholder from "~/components/layout/building";
+
 export default function Index() {
-  return (
-    <div className="flex h-full w-full p-14 items-center justify-center text-4xl">
-      ⛑️ Oops! This page is still under construction...
-    </div>
-  );
+  return <BuildingPlaceholder />;
 }
